55530491a7bd27f2d097af51b0c1048d391448d4
Saad Ullah Khan <saaad.khaan.69@gmail.com>
2024-06-07 16:31:35 +0500

    back fix

--- my-app/src/components/dashboard/components/sep-section.js
+++ my-app/src/components/dashboard/components/sep-section.js
@@ -225,7 +225,19 @@ export default function Details(props) {
       <div>
         {(!val || add >= 1) && (
           <div className="flex mx-96">
-            <button className="block w-24 rounded-md bg-gray-800  py-2.5 text-center text-sm  text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mx-auto mt-10">
+            <button className="flex px-7 rounded-md bg-gray-800  py-2.5 text-center text-sm  text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mx-auto mt-10">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="size-4 mr-1"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.793 2.232a.75.75 0 0 1-.025 1.06L3.622 7.25h10.003a5.375 5.375 0 0 1 0 10.75H10.75a.75.75 0 0 1 0-1.5h2.875a3.875 3.875 0 0 0 0-7.75H3.622l4.146 3.957a.75.75 0 0 1-1.036 1.085l-5.5-5.25a.75.75 0 0 1 0-1.085l5.5-5.25a.75.75 0 0 1 1.06.025Z"
+                  clipRule="evenodd"
+                />
+              </svg>
               Back
             </button>
             <button
